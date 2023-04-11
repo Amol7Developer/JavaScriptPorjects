cab8a6cf2db6de13d79c09a40cc766db46911a93
Amol7Developer <amol0783com@gmail.com>
2023-04-11 23:54:33 +0530

    Quiz-app project html, css, and Js file uploaded

---
+++ quiz-app/script.js
@@ -0,0 +1,13 @@
+const quizData = [
+    {
+        question: 'How old is Arya?',
+        a: '10',
+        b: '17',
+        c: '26',
+        d: '110',
+        correct: 'c'
+    },{
+        question: 'What is the best programming language?',
+        a:''
+    }
+]
