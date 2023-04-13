52610360f44e355962496a810aff0fea0b49544f
Amol7Developer <amol0783com@gmail.com>
2023-04-13 23:53:25 +0530

    Quiz app created and html, css, and Js file code updated as per app looks

--- quiz-app/script.js
+++ quiz-app/script.js
@@ -36,7 +36,8 @@ const quizData = [
         correct:'b'
     }
 ]
-
+const quiz = document.getElementById("quiz");
+const answerEls = document.querySelectorAll(".answer");
 const questionE1 = document.getElementById('question');
 const a_text = document.getElementById('a_text'); 
 const b_text = document.getElementById('b_text');
@@ -45,10 +46,12 @@ const d_text = document.getElementById('d_text');
 const submitBtn = document.getElementById('submit');
 
 let currentQuiz = 0;
+let score =0;
 
 loadQuiz();
 
 function loadQuiz() {
+    deselectAnswers();
     const currentQuizData = quizData[currentQuiz];
 
     questionE1.innerText = currentQuizData.question;
@@ -57,28 +60,47 @@ function loadQuiz() {
     c_text.innerText = currentQuizData.c;
     d_text.innerText = currentQuizData.d;
 
-    
-
 }
 
 function getSelected() {
-    const answers = document.querySelectorAll(".answer");
+    
+    let answer = undefined;
 
-    answers.forEach((answers) =>{
-        console.log(answers.value);
+    answerEls.forEach((answerEl) =>{
+       // console.log(answers.value);
+        if(answerEl.checked){
+            answer = answerEl.id;
+        }
     });
+    return answer;
+}
+
+function deselectAnswers(){
+    answerEls.forEach((answerEl) =>{
+             answerEl.checked = false;
+        });
 }
 
 submitBtn.addEventListener('click',() =>{
-    currentQuiz++;
+    // currentQuiz++;
 
-    getSelected();
+    const answer = getSelected();
 
-    // if(currentQuiz < quizData.length){
-    //     loadQuiz();
-    // }else{
-    //     //TODO: Show results
-    //     alert('You finished! Get yourself an Orange Lemonade');
-    // }
+    if(answer){
+        if(answer === quizData[currentQuiz].correct){
+            score++;
+        }
+
+        currentQuiz++;
+        if(currentQuiz < quizData.length){
+             loadQuiz();
+         }else{
+             //TODO: Show results
+            //alert('You finished! Get yourself an Orange Lemonade');
+             quiz.innerHTML = `<h2>You answered correctly at ${score}/${quizData.length} question.</h2>
+             <button onclick="location.reload()">Reload</button>`;   
+
+        }
+    }
 
 });
