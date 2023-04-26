3ee7a8c6b81cc5aff3923419f46455b956f7cb10
Amol7Developer <amol0783com@gmail.com>
2023-04-26 19:54:53 +0530

    code updated html, css, and javascript file

--- movie-app/script.js
+++ movie-app/script.js
@@ -2,6 +2,8 @@ const APIKEY = "04c35731a5ee918f014970082a0088b1";
 const APIURL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page1";
 const IMGPATH = "https://image.tmdb.org/t/p/w1280";
 
+const main = document.querySelector("main");
+
 async function getMovies() {
     const resp = await fetch(APIURL);
     const respData = await resp.json();
@@ -15,7 +17,40 @@ async function getMovies() {
     //     document.body.appendChild(img);
     // });
 
+
+    respData.results.forEach(movie =>{
+        const {poster_path, title, vote_average} = movie;
+
+        const movieEl = document.createElement("div");
+        movieEl.classList.add("movie");
+
+        movieEl.innerHTML =  `
+         <img 
+                src="${IMGPATH + poster_path}"
+                alt="${title}" 
+            />
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getClassByRate(vote_average)}">
+                    ${vote_average}</span>
+             </div>
+        
+        `;  
+
+        main.appendChild(movieEl);
+    });
+
     return respData;
+}
+
+function getClassByRate(vote){
+    if(vote >= 8){
+        return "green";
+    }else if(vote >= 5){
+        return "orange";
+    }else{
+        return "red";
+    }
 }
 
 getMovies();
